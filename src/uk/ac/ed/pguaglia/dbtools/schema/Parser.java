3a8adb7f74ff2f38cad7a39b83f6b4d2aec412ef
Paolo Guagliardo <paolo.guagliardo@ed.ac.uk>
2019-08-26 17:27:42 +0100

    Improved parser with informative error messages

--- src/uk/ac/ed/pguaglia/dbtools/schema/Parser.java
+++ src/uk/ac/ed/pguaglia/dbtools/schema/Parser.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.cli.ParseException;
+
 public class Parser {
 
 	public static final String ATTRIBUTE_SEPARATOR = ",";
@@ -12,48 +14,48 @@ public class Parser {
 	public static final String ATTRIBUTE_REGEX = "[a-zA-Z]\\w*";
 	public static final String LEFT_RIGHT_SEP = "->";
 
-	private String attrListRegex = String.format("%s(%s%s)*", Parser.ATTRIBUTE_REGEX, Parser.ATTRIBUTE_SEPARATOR, Parser.ATTRIBUTE_REGEX);
+	private String attrListRegex = String.format( "\\s*%s\\s*(\\s*%s\\s*%s\\s*)*",
+			Parser.ATTRIBUTE_REGEX, Parser.ATTRIBUTE_SEPARATOR, Parser.ATTRIBUTE_REGEX );
 	private String fdRegex = String.format("%s%s%s", attrListRegex, Parser.LEFT_RIGHT_SEP, attrListRegex);
-	private String fdListRegex = String.format("%s(%s%s)*", fdRegex, Parser.FD_SEPARATOR, fdRegex);
 
-	public Parser () {
+
+	public String parseAttribute(String input) throws ParseException {
+		input = input.trim();
+		if (input.matches(ATTRIBUTE_REGEX) == false) {
+			throw new ParseException(String.format("Invalid attribute name: \"%s\"", input));
+		}
+		return input;
 	}
 
-	public List<String> parseAttributeList(String input) throws Exception {
-		input = input.replaceAll("\\s+", "");
-		if (input.matches(attrListRegex) == false) {
-			throw new Exception();
-		}
+	public List<String> parseAttributeList(String input) throws ParseException {
 		ArrayList<String> attributes = new ArrayList<String>();
-		for (String a : Arrays.asList(input.split(Parser.ATTRIBUTE_SEPARATOR))) {
-			attributes.add(a);
+		for (String a : Arrays.asList(input.split(Parser.ATTRIBUTE_SEPARATOR, -1))) {
+			attributes.add(parseAttribute(a));
 		}
 		return attributes;
 	}
 
-	public Set<String> parseAttributeSet(String input) throws Exception {
+	public Set<String> parseAttributeSet(String input) throws ParseException {
 		return new HashSet<String>(parseAttributeList(input));
 	}
 
-	public Set<FunctionalDependency> parseFDSet(String input) throws Exception {
-		input = input.replaceAll("\\s+", "");
-		if (input.matches(fdListRegex) == false) {
-			throw new Exception();
-		}
-		String[] fdsList = input.split(Parser.FD_SEPARATOR);
+	public Set<FunctionalDependency> parseFDSet(String input) throws ParseException {
+		return parseFDSet(input.split(Parser.FD_SEPARATOR, -1));
+	}
+
+	public Set<FunctionalDependency> parseFDSet(String[] input) throws ParseException {
 		HashSet<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
-		for (String fd : fdsList) {
+		for (String fd : input) {
 			fds.add(parseFunctionalDependency(fd));
 		}
 		return fds;
 	}
 
-	public FunctionalDependency parseFunctionalDependency(String input) throws Exception {
-		input = input.replaceAll("\\s+", "");
+	public FunctionalDependency parseFunctionalDependency(String input) throws ParseException {
 		if (input.matches(fdRegex) == false) {
-			throw new Exception();
+			throw new ParseException(String.format("Cannot parse functional dependency: \"%s\"", input));
 		}
-		String[] attrLists = input.split(Parser.LEFT_RIGHT_SEP);
+		String[] attrLists = input.split(Parser.LEFT_RIGHT_SEP, -1);
 		return new FunctionalDependency(parseAttributeSet(attrLists[0]),parseAttributeSet(attrLists[1]));
 	}
 }
